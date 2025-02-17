af4626d7f4d6f213d1fa3536800cc9f2be4a0ebf
RozeJa <92266408+RozeJa@users.noreply.github.com>
2025-02-17 11:26:09 +0100

    Arithmetic operations on Zlomek

--- zlomky/src/main/java/cz/uhk/zlomky/ZlomkyApp.java
+++ zlomky/src/main/java/cz/uhk/zlomky/ZlomkyApp.java
@@ -9,7 +9,16 @@ public class ZlomkyApp {
 
         // vytvoreni instance(=objektu) tridy Zlomek
         Zlomek a = new Zlomek(3, 4);
-
         System.out.printf("Zlomek a = %s\n", a);
+
+        Zlomek b = new Zlomek(2, 5);
+        System.out.printf("Zlomek b = %s\n", b);
+
+        Zlomek c = a.krat(b);
+        System.out.printf("Zlomek a * b = %s\n", c.zkrat());
+        System.out.printf("Zlomek a / b = %s\n", a.deleno(b).zkrat());
+        System.out.printf("Zlomek a + b = %s\n", a.plus(b));
+        System.out.printf("Zlomek a - b = %s\n", a.minus(b));
+
     }
 }
--- zlomky/src/main/java/cz/uhk/zlomky/model/Zlomek.java
+++ zlomky/src/main/java/cz/uhk/zlomky/model/Zlomek.java
@@ -1,8 +1,8 @@
 package cz.uhk.zlomky.model;
 
 public class Zlomek {
-    private int citatel;
-    private int jmenovatel;
+    private final int citatel;
+    private final int jmenovatel;
 
     public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
@@ -14,6 +14,49 @@ public class Zlomek {
     }
     public int getJmenovatel() {
         return jmenovatel;
+    }
+
+    public Zlomek krat(Zlomek druhy) {
+        return new Zlomek(citatel * druhy.citatel, jmenovatel * druhy.jmenovatel);
+    }
+
+    public Zlomek deleno(Zlomek delitel) {
+        return new Zlomek(citatel * delitel.jmenovatel, jmenovatel * delitel.citatel);
+    }
+
+    public Zlomek plus(Zlomek druhy) {
+        int citatel = this.citatel * druhy.jmenovatel + druhy.citatel * this.jmenovatel;
+        int jmenovatel = this.jmenovatel * druhy.jmenovatel;
+
+        return new Zlomek(citatel , jmenovatel);
+    }
+
+    public Zlomek minus(Zlomek mencitel) {
+        int citatel = this.citatel * mencitel.jmenovatel - mencitel.citatel * this.jmenovatel;
+        int jmenovatel = this.jmenovatel * mencitel.jmenovatel;
+        
+        return new Zlomek(citatel , jmenovatel);
+    }
+
+    public Zlomek zkrat() {
+
+        int nejvetsiSpolecnyDelitel;
+        if (citatel > jmenovatel) {
+            nejvetsiSpolecnyDelitel = najdiNejvetsihoSpolecnyhoDelitele(citatel, jmenovatel);
+        } else {
+            nejvetsiSpolecnyDelitel = najdiNejvetsihoSpolecnyhoDelitele(jmenovatel, citatel);
+        }
+
+        return new Zlomek(citatel / nejvetsiSpolecnyDelitel, jmenovatel / nejvetsiSpolecnyDelitel);
+    }
+
+    private int najdiNejvetsihoSpolecnyhoDelitele(int a, int b) {
+        int zbytekPoDeleni = a % b;
+        if (zbytekPoDeleni == 0) {
+            return b;
+        } else {
+            return najdiNejvetsihoSpolecnyhoDelitele(b, zbytekPoDeleni);
+        }
     }
 
     @Override
